f62440aa28a3c4d33ca7aa7622b83d88eecb7653
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-07-03 00:33:11 +0900

    ロガーを実装

---
+++ src/lib/logger/logger.ts
@@ -0,0 +1,19 @@
+export enum LogLevel {
+  Info,
+  Error,
+}
+
+export class logger {
+  static log(message: string, level: string = "LOG", color = "") {
+    const timestamp = new Date().toISOString();
+    console.log(`\n${color}[${level}]:${message}\x1b[0m -> ${timestamp}\n`);
+  }
+
+  static info(message: string) {
+    this.log(message, "INFO", "\x1b[32m");
+  }
+
+  static error(message: string) {
+    this.log(message, "ERROR", "\x1b[33m");
+  }
+}
