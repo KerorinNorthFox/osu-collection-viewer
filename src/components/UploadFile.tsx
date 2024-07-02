a4719000f16afe5698daa4b2f0dd8ef89c0742f1
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-07-02 10:01:56 +0900

    準備

---
+++ src/components/UploadFile.tsx
@@ -0,0 +1,5 @@
+const UploadFile = () => {
+  return <div></div>;
+};
+
+export default UploadFile;
---
+++ src/components/header/Header.tsx
@@ -0,0 +1,11 @@
+const Header = () => {
+  return (
+    <div className="w-screen h-header px-4 fixed shadow-md">
+      <div className="h-full flex items-center">
+        <p className="">Osu! FC Checker</p>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
