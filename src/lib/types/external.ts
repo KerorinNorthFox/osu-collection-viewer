4c7bf8e1086efcbe4df43477a40e91ed486bb6a8
kerorinnf <kitakitsune.gingisune.8220A@gmail.com>
2024-07-17 15:32:42 +0900

    NotifyToastのデザイン変更＆externalの各型の説明を追加

--- src/lib/types/external.ts
+++ src/lib/types/external.ts
@@ -1,3 +1,11 @@
+// apiから得られる生の譜面データ
+// @params
+// osu_file_name:
+// beatmap_id: 譜面ID
+// beatmapset_id: 譜面の属するセットのid
+// md5: 譜面のMD5
+// ranked_status: 譜面のランク状態
+// mode: 譜面のゲームモード
 export interface BeatmapRawData {
   osu_file_name: string;
   beatmap_id: number;
@@ -7,23 +15,37 @@ export interface BeatmapRawData {
   mode: number;
 }
 
+// テーブルに表示する譜面データ
 export interface BeatmapData extends BeatmapRawData {
   user_combo: number;
   max_combo: number;
 }
 
+// コレクションのデータ
+// @params
+// name: コレクション名
+// beatmapsCount: 譜面数
+// beatmapsMd5: 譜面のMD5
 export interface CollectionData {
   name: string;
   beatmapsCount: number;
   beatmapsMd5: string[];
 }
 
+// osu!.dbから得られるデータ
+// username: ユーザー名
+// beatmaps_count: 譜面総数
+// beatmaps: 譜面のデータ
 export interface OsuDB {
   username: string;
   beatmaps_count: string;
   beatmaps: BeatmapRawData[];
 }
 
+// collectino.dbから得られるデータ
+// @params
+// collectionscount: コレクションの総数
+// collection: コレクションの実データ
 export interface OsuCollectionDB {
   collectionscount: number;
   collection: CollectionData[];
